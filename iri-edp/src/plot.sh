b4126d80bb49b06654f4a032f0e2ab8012fed13d
zmoon <zmoon92@gmail.com>
2025-05-20 15:34:58 -0500

    Script to plot EDP with gnuplot

---
+++ iri-edp/src/plot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Run and plot the two cases with gnuplot
+
+set -eu
+
+make -j 2
+
+./iri -c 1 > case1.csv
+./iri -c 2 > case2.csv
+
+gnuplot -p -e " \
+  set datafile separator ','; \
+  set title 'Electron density profiles'; \
+  set xlabel 'n_e [m^{-3}]'; \
+  set ylabel 'altitude [km]'; \
+  plot 'case1.csv' using 2:1 with lines title 'case 1', \
+       'case2.csv' using 2:1 with lines title 'case 2'; \
+  "
+
