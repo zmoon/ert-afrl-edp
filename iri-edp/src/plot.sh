eb0a23a5569dc174642f8cc8dd593787e0581afe
zmoon <zmoon92@gmail.com>
2025-05-20 16:14:26 -0500

    More plots

--- iri-edp/src/plot.sh
+++ iri-edp/src/plot.sh
@@ -11,10 +11,31 @@ make -j 2
 
 gnuplot -p -e " \
   set datafile separator ','; \
-  set title 'Electron density profiles'; \
+  set title 'Electron density profiles at 37.8°N, 75.4°W'; \
   set xlabel 'n_e [m^{-3}]'; \
   set ylabel 'altitude [km]'; \
-  plot 'case1.csv' using 2:1 with lines title 'case 1', \
-       'case2.csv' using 2:1 with lines title 'case 2'; \
+  plot 'case1.csv' using 2:1 with lines title '2021-03-03 11:00 UTC', \
+       'case2.csv' using 2:1 with lines title '2021-03-04 23:00 UTC'; \
   "
 
+gnuplot -p -e " \
+  set datafile separator ','; \
+  set title 'Temperature profiles at 37.8°N, 75.4°W'; \
+  set xlabel 'temperature [K]'; \
+  set ylabel 'altitude [km]'; \
+  plot 'case1.csv' using 3:1 with lines title '2021-03-03 11:00 UTC T_n', \
+       '' using 4:1 with lines title 'T_i', \
+       '' using 5:1 with lines title 'T_e', \
+       'case2.csv' using 3:1 with lines title '2021-03-04 23:00 UTC T_n', \
+       '' using 4:1 with lines title 'T_i', \
+       '' using 5:1 with lines title 'T_e', \
+  "
+
+gnuplot -p -e " \
+  set datafile separator ','; \
+  set title 'PF/GF profiles at 37.8°N, 75.4°W'; \
+  set xlabel 'PF/GF [1]'; \
+  set ylabel 'altitude [km]'; \
+  plot 'case1.csv' using 13:1 with lines title '2021-03-03 11:00 UTC', \
+       'case2.csv' using 13:1 with lines title '2021-03-04 23:00 UTC'; \
+  "
