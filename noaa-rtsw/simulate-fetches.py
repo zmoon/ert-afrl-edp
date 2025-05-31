04d35ee67e218303f21ac6348d8cb36b1ad306d0
zmoon <zmoon92@gmail.com>
2025-05-31 16:19:03 -0500

    Pretend to make many hourly fetches

---
+++ noaa-rtsw/simulate-fetches.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S uv run --script
+"""
+Simulate making multiple 1-hour fetches over time using the 7-day file.
+"""
+# /// script
+# dependencies = [
+#   "numpy",
+#   "pandas ~=2.2",
+#   "pyarrow",
+#   "requests",
+# ]
+# ///
+
+import pandas as pd
+
+from rtsw import get, save
+
+
+df = get(hour=False)
+
+tmin = df["time_tag"].min()
+tmax = df["time_tag"].max()
+times = pd.date_range(tmin.floor("h"), tmax, freq="1h")
+
+for a, b in zip(times[:-1], times[1:]):
+    df_ab = df[df.time_tag.between(a, b, inclusive="left")].reset_index()
+    df_ab.attrs["retrieved_at"] = str(b)
+    p = save(df_ab)
+    print(p.as_posix())
